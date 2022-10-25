5e2e0534f20379ba02423153532d7875f1cbba53
MarouaneBenbadelkader <marouane.benabdelkader@usmba.ac.ma>
2022-10-25 09:26:10 +0100

    Add project files.

---
+++ Exercice1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Exercice1
+{   
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            int prem = 0;
+       
+            do
+            {
+                Console.WriteLine("entrez un nombre");
+                prem = Console.Read()
+            } while ();
+            
+            
+        }
+    }
+}
